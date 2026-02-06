9d424d45b835ae5877f635f6ca2cd54d62b0716e
LsFbr <louisfabre@hotmail.fr>
2026-02-06 13:25:59 +0100

    refactor(tests/unit/test_server.py): update tests for bug "Point updates are not reflected" to real unit test

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -43,7 +43,6 @@ def test_showSummary_with_unknown_email(mocker):
 
 
 # tests Clubs_should_not_be_able_to_use_more_than_their_points_allowed
-
 def test_purchasePlaces_success(mocker):
     """Test purchasePlaces avec un nombre de points suffisants"""
     initial_club_points = "24"
@@ -229,110 +228,33 @@ def test_purchasePlaces_with_past_competition(mocker):
     mock_render.assert_called_once_with("welcome.html", club=club, competitions=[competition], datetime=server.datetime)
     mock_flash.assert_called_once_with("Cannot book places for past competitions")
 
-# purchasePlaces tests
+# tests Point_updates_are_not_reflected
+def test_purchasePlaces_points_and_places_deduction(mocker):
+    '''Test que les points et les places sont correctement déduits après une réservation réussie.'''
+    initial_club_points = "24"
+    initial_competition_numberOfPlaces = "25"
+    club = {"name": "Club Example", "email": "club@example.com", "points": initial_club_points}
+    competition = {"name": "Competition Example", "date": "2999-03-27 10:00:00", "numberOfPlaces": initial_competition_numberOfPlaces}
+    places_to_book = "2"
 
-#def test_purchasePlaces_points_deduction(mocker, client):
-#    """
-#    Test que les points sont correctement déduits après une réservation réussie.
-#    Vérifie que les points sont déduits dans server.clubs (en mémoire)
-#    Vérifie que les bonnes données sont passées à updateClubs() (pour sauvegarde JSON)
-#    """
-#    mock_update_clubs = mocker.patch('server.updateClubs')
-#    mocker.patch('server.updateCompetitions')
-#    
-#    # Sauvegarder l'état initial depuis server.clubs (après setup_mock_data)
-#    club_before_deduction = next(club for club in server.clubs if club['name'] == 'She Lifts')
-#    points_before_deduction = int(club_before_deduction['points'])
-#    places_to_book = 3
-#    
-#    response = client.post('/purchasePlaces', data={
-#        'competition': 'Fall Classic',
-#        'club': 'She Lifts',
-#        'places': str(places_to_book)
-#    })
-#    
-#    assert response.status_code == 200
-#    assert b'Great-Booking of 3 places for Fall Classic has been completed!' in response.data
-#    
-#    # Vérification 1 : Les données passées à updateClubs() sont correctes (pour sauvegarde JSON)
-#    mock_update_clubs.assert_called_once()
-#    clubs_passed = mock_update_clubs.call_args[0][0] # mock_update_clubs.call_args = (args=(clubs,), kwargs={}) => clubs_passed = clubs
-#    club_after_deduction = next(club for club in clubs_passed if club['name'] == 'She Lifts')
-#    points_after_deduction = int(club_after_deduction['points'])
-#    assert points_after_deduction == points_before_deduction - places_to_book
-#    
-#    # Vérification 2 : Les données dans server.clubs sont mises à jour (en mémoire)
-#    club_in_memory = next(club for club in server.clubs if club['name'] == 'She Lifts')
-#    assert int(club_in_memory['points']) == points_before_deduction - places_to_book
+    mock_update_clubs = mocker.patch('server.updateClubs')
+    mock_update_competitions = mocker.patch('server.updateCompetitions')
+    mocker.patch.object(server, "clubs", [club])
+    mocker.patch.object(server, "competitions", [competition])
 
+    with server.app.test_request_context(method="POST", data={
+        "competition": competition["name"],
+        "club": club["name"],
+        "places": places_to_book
+    }):
+        server.purchasePlaces()
 
-#def test_purchasePlaces_competition_places_deduction(mocker, client):
-#    """
-#    Test que les places de la compétition sont correctement déduites.
-#    Vérifie que les places sont déduites dans server.competitions (en mémoire)
-#    Vérifie que les bonnes données sont passées à updateCompetitions() (pour sauvegarde JSON)
-#    """
-#    mocker.patch('server.updateClubs')
-#    mock_update_competitions = mocker.patch('server.updateCompetitions')
-#    
-#    # Sauvegarder l'état initial depuis server.competitions (après setup_mock_data)
-#    comp_before_deduction = next(comp for comp in server.competitions if comp['name'] == 'Spring Festival')
-#    places_before_deduction = int(comp_before_deduction['numberOfPlaces'])
-#    places_to_book = 2
-#    
-#    response = client.post('/purchasePlaces', data={
-#        'competition': 'Spring Festival',
-#        'club': 'Simply Lift',
-#        'places': str(places_to_book)
-#    })
-#    
-#    assert response.status_code == 200
-#            
-#    # Vérifie que les données passées à updateCompetitions() sont correctes (pour sauvegarde JSON)
-#    mock_update_competitions.assert_called_once()
-#    competitions_passed = mock_update_competitions.call_args[0][0] # mock_update_competitions.call_args = (args=(competitions,), kwargs={}) => competitions_passed = competitions
-#    comp_after_deduction = next(comp for comp in competitions_passed if comp['name'] == 'Spring Festival')
-#    places_after_deduction = int(comp_after_deduction['numberOfPlaces'])
-#    assert places_after_deduction == places_before_deduction - places_to_book
-#    
-#    # Vérifie que les données dans server.competitions sont mises à jour (en mémoire)
-#    comp_in_memory = next(comp for comp in server.competitions if comp['name'] == 'Spring Festival')
-#    assert int(comp_in_memory['numberOfPlaces']) == places_before_deduction - places_to_book
-#
-#
-#def test_purchasePlaces_other_clubs_not_affected(mocker, client):
-#    """
-#    Test que les autres clubs ne sont pas affectés par une réservation.
-#    Vérifie que les clubs non modifiés sont inchangés dans server.clubs.
-#    """
-#
-#    mock_update_clubs = mocker.patch('server.updateClubs')
-#    mocker.patch('server.updateCompetitions')
-#    
-#    clubs_before = {club['name']: int(club['points']) for club in server.clubs}
-#    
-#    response = client.post('/purchasePlaces', data={
-#        'competition': 'Spring Festival',
-#        'club': 'Simply Lift',
-#        'places': '2'
-#    })
-#    
-#    assert response.status_code == 200
-#    
-#    mock_update_clubs.assert_called_once()
-#    clubs_passed = mock_update_clubs.call_args[0][0] 
-#    clubs_after_dict = {}
-#    for club in clubs_passed:
-#        clubs_after_dict[club['name']] = int(club['points'])
-#    
-#    #club modifié : Simply Lift
-#    assert clubs_after_dict['Simply Lift'] == clubs_before['Simply Lift'] - 2
-#
-#    #clubs non modifiés : Iron Temple et She Lifts
-#    assert clubs_after_dict['Iron Temple'] == clubs_before['Iron Temple']
-#    assert clubs_after_dict['She Lifts'] == clubs_before['She Lifts']
-#
-#
+    assert club["points"] == str(int(initial_club_points) - int(places_to_book))
+    mock_update_clubs.assert_called_once_with([club])
+    print(club["points"])
+    assert competition["numberOfPlaces"] == str(int(initial_competition_numberOfPlaces) - int(places_to_book))
+    mock_update_competitions.assert_called_once_with([competition])
+    print(competition["numberOfPlaces"])
 
 # points_board tests
 #def test_points_board_accessible_without_authentication(client):
